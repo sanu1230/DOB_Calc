3dcbade55da6af7ea856d50285956c4d1e98b043
Santosh Kamble <sanu12300@gmail.com>
2021-12-26 11:50:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 pred = 0
 note = 0
 
-@app.route('/', methods = ['POST'])
+@app.route('/', methods = ['POST', 'GET'])
 def age():
     global pred, note
     if request.method == 'POST':
